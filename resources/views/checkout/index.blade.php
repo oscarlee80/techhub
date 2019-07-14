352555ca1062da724513fbd32c08cc536c2bdee2
Tomas Monaco <monacotomas99gmail.com>
2019-07-14 12:10:38 -0300

    checkout

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -20,7 +20,7 @@
             @foreach ($products as $product)
                     <div class="row">
                         <div class="col-12 col-sm-12 col-md-2 text-center">
-                            <img class="img-responsive" src="{{asset('/storage/products/' . $product->defaultImage())}}" alt="prewiew" width="120" height="80">
+                            <img class="img-responsive" src="{{asset('/storage/products/' . $product->dafaultPhoto)}}" alt="prewiew" width="120" height="80">
                         </div>
                         <div class="col-12 text-sm-center col-sm-12 text-md-left col-md-6">
                             <h4 class="product-name"><strong>{{$product->title}}</strong></h4>
